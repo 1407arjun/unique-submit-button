6d9db8844072b7cf87d3f2fce74f4c1433b4a8d8
Arjun <arjun140702@gmail.com>
2022-10-27 18:31:13 +0530

    Implement basic CSS and JavaScript logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import { Center, Heading } from "@chakra-ui/react"
+import { Center, Button } from "@chakra-ui/react"
 import type { NextPage } from "next"
+import { useEffect, useState } from "react"
 import Head from "../components/Head"
+
+const onHover = () => {
+    let boolean = true
+    const button = document.querySelector("button")
+
+    if (button)
+        button.addEventListener("mouseover", (ev) => {
+            console.log(1)
+        })
+}
 
 const Home: NextPage = () => {
     return (
         <Center minH="100vh">
             <Head />
-            <Heading>Welcome to Next.js</Heading>
+            <Button>Submit</Button>
         </Center>
     )
 }

91a08e07f263aa28fa78de1a3327ca5c50e108cf
Arjun <arjun140702@gmail.com>
2022-10-27 18:39:01 +0530

    Complete animation implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
 import { Center, Button } from "@chakra-ui/react"
 import type { NextPage } from "next"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Head from "../components/Head"
 
-const onHover = () => {
-    let boolean = true
-    const button = document.querySelector("button")
-
-    if (button)
-        button.addEventListener("mouseover", (ev) => {
-            console.log(1)
-        })
-}
-
 const Home: NextPage = () => {
+    const [move, setMove] = useState(true)
+
+    const onHover = () => {
+        const button = document.querySelector("#submit") as HTMLButtonElement
+
+        if (button) {
+            button.style.animationPlayState = "paused"
+            if (move) button.style.animationName = "move"
+            else button.style.animationName = "move-back"
+            button.style.animationPlayState = "running"
+            setMove(!move)
+        }
+    }
+
     return (
         <Center minH="100vh">
             <Head />
-            <Button>Submit</Button>
+            <Button id="submit" onMouseOver={onHover}>
+                Submit
+            </Button>
         </Center>
     )
 }

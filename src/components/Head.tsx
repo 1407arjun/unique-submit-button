3f6a58b11fd42f599bb7452b892915e0e30e3a32
Arjun <arjun140702@gmail.com>
2022-10-27 22:13:20 +0530

    Update SEO tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head"
 
 const PageHead = () => {
-    const title = "Page Title"
-    const desc = "Page Desc"
-    const endpoint = "https://example.com"
-    const domain = "example.com"
-    const siteName = "Page Name"
-    const twitterHandle = ""
+    const title = "Unique Submit Button"
+    const desc =
+        "Implementation of the moving submit button as seen on Instagram Reels"
 
     return (
         <Head>
@@ -19,21 +16,6 @@ const PageHead = () => {
             <title>{title}</title>
             <meta name="description" content={desc} />
             <link rel="icon" href="/favicon.ico" />
-
-            <meta property="og:title" content={title} />
-            <meta property="og:type" content="website" />
-            <meta property="og:description" content={desc} />
-            <meta property="og:image" content={`${endpoint}/ogicon.png`} />
-            <meta property="og:url" content={endpoint} />
-            <meta property="og:site_name" content={siteName} />
-
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content={twitterHandle} />
-            <meta property="twitter:domain" content={domain} />
-            <meta property="twitter:url" content={endpoint} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={desc} />
-            <meta name="twitter:image" content={`${endpoint}/ogicon.png`} />
         </Head>
     )
 }
